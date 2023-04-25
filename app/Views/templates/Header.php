810bc733076f629952f6cac4cac8ad1e17edb0f6
Xaclaxablau <klebersonmoura76@gmail.com>
2023-04-25 16:51:11 -0300

    UP

--- app/Views/templates/Header.php
+++ app/Views/templates/Header.php
@@ -26,6 +26,8 @@
     <link rel="stylesheet" href="../assets/plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
     <link rel="stylesheet" href="../assets/plugins/summernote/summernote-bs4.min.css">
+    <!-- FullCallendar -->
+    <link rel="stylesheet" href="../assets/plugins/fullcalendar/main.css">
     <!-- Extras CSS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/iconify/2.0.0/iconify.min.js"></script>
     <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
@@ -38,6 +40,9 @@
     <!-- Extras -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.2/axios.min.js" integrity="sha512-QTnb9BQkG4fBYIt9JGvYmxPpd6TBeKp6lsUrtiVQsrJ9sb33Bn9s0wMQO9qVBFbPX3xHRAsBHvXlcsrnJjExjg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+    <script src="../assets/plugins/fullcalendar/main.js"></script>
+    <script src="../assets/plugins/fullcalendar/locales-all.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2.7.13/dist/vue.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.7/dist/loadingoverlay.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/downloadjs/1.4.8/download.min.js"></script>
